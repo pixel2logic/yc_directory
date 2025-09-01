3417174b9dc624cb203af68149f2dcecb665eaf3
pixel2logic <khokhariyapritesh123@gmail.com>
2025-09-01 13:00:41 +0530

    Fix Image src type issue with fallback

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -27,8 +27,8 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
           </div>
 
           <Image
-            src={user.image}
-            alt={user.name}
+            src={user.image ?? "/default-avatar.png"}
+            alt={user.name ?? "User"}
             width={220}
             height={220}
             className="profile_image"
